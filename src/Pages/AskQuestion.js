276d3a86ed36ef603041d7f539e7ca76d380f54c
부들이 <84694944+namnam-5799@users.noreply.github.com>
2022-12-22 17:58:40 +0900

    메인 페이지 1차 완성

--- src/Pages/AskQuestion.js
+++ src/Pages/AskQuestion.js
@@ -1,14 +1,26 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from '../Components/Header';
+import { useEffect, useState,useRef } from 'react';
+import { StacksEditor } from "@stackoverflow/stacks-editor";
+import '@stackoverflow/stacks-editor/dist/styles.css';
+import '@stackoverflow/stacks';
+import '@stackoverflow/stacks/dist/css/stacks.css';
+import Footer from '../Components/Footer';
+
+// const GlobalStyle = createGlobalStyle`
+// 	* {
+// 		box-sizing: content-box;
+// 	}
+// `;
+
 const backgroundImg = 'https://cdn.sstatic.net/Img/ask/background.svg?v=2e9a8205b368';
 
 const Container = styled.div`
 	width: 100%;
-	height: 100vh;
+	height: 180vh;
 	background-color: #f8faf9;
 	display: flex;
 	flex-direction: column;
-
 `;
 
 const Box = styled.div`
@@ -39,10 +51,6 @@ const Main = styled.div`
     color: #232629;
   }
 
-  /* > img{
-    width: 600px;
-    height: 130px;
-  } */
 `
 
 const BlueBox = styled.div`
@@ -52,7 +60,9 @@ const BlueBox = styled.div`
   border: 1px solid rgb(184, 201, 216);
   background-color: rgb(235, 244, 251);
   margin-top: 20px;
-  border-radius: 5px;
+  border-radius: 5px; 
+  margin-bottom: 15px;
+  box-sizing: content-box;
 
   > h2{
     display: block;
@@ -68,22 +78,179 @@ const BlueBox = styled.div`
     font-size: 15px;
     color: #3a4045;
   }
-  >p:nth-child(4){
+  >p:nth-child(3){
+    margin-bottom:10px
+  }
+  > p:nth-child(4){
     font-size: 14px;
     font-weight: 600;
     margin-bottom: 12px;
   }
-  >ul >li{
+
+  & .font-blue{
+    color: #0074cc;
+  }
+  > ul > li{
     font-size: 13px;
     margin-left: 28px;
     color: #3a4045;
   }
 `
 
+const TitleBox = styled.div`
+  width: 801px;
+  height: 130px;
+  padding: 24px;
+  border: 1px solid rgb(227, 230, 230);
+  background-color: rgb(255, 255, 255);
+  border-radius: 5px; 
+  margin-bottom: 15px;
+  box-sizing: content-box;
+
+  >h5{
+    font-size: 14px;
+  }
+
+  > p{
+    font-size: 12px;
+    color: #3e3f42;
+    padding: 6px 0px;
+  }
+
+  // next 버튼 없는 버전 
+  /* >input{
+    width: 780px;
+    height: 15px;
+    padding: 7px 9px;
+    border: 1px solid rgb(189, 190, 193) ;
+    border-radius: 4px; 
+  } */
+
+  //next버튼 있는 버전 
+  > input{
+    width: 780px;
+    height: 18px;
+    padding: 7px 9px;
+    border: 1px solid rgb(189, 190, 193) ;
+    border-radius: 4px; 
+    margin-bottom: 14px;
+  }
+
+  >input::placeholder{
+    color: rgb(191, 193, 196);
+  }
+  >input:focus{
+    outline-color: hsl(206deg 90% 70%);
+    box-shadow: 0px 0px 6px skyblue;
+    border-radius: 5px;
+  }
+
+  > button{
+  width: 30px;
+  height:14px;
+  box-shadow:inset 0px 1px 0px 0px #97c4fe;
+	background-color:#0A95FF;
+	border-radius:3px;
+	border:1px solid rgb(34, 147, 237);;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:13px;
+	font-weight:400;
+	padding: 10.4px;
+	text-decoration:none;
+  }
+`
+
+const QuestionBox = styled.div`
+  width: 801px;
+  height: 369px;
+  padding: 24px;
+  border: 1px solid rgb(227, 230, 230);
+  background-color: rgb(255, 255, 255);
+  border-radius: 5px;
+  box-sizing: content-box; 
+  margin-bottom: 12px;
+
+  >h5{
+    font-size: 15px;
+    margin-bottom: 3px;
+  }
+  >p{
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
+`
+const Editor = styled.div`
+  width:799px;
+  height: 244px;
+  padding: 6px 2px;
+  margin-bottom: 10px;
+`
+const BlueButton = styled.button`
+  width: 149px;
+  height:36px;
+  box-shadow:inset 0px 1px 0px 0px #97c4fe;
+	background-color:#0A95FF;
+	border-radius:3px;
+	border:1px solid rgb(34, 147, 237);;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:13px;
+	font-weight:400;
+	padding: 10.4px;
+	text-decoration:none;
+`
+const DiscardButton = styled.button`
+  width: 100px;
+  height:36px;
+  margin-left: 10px;
+	background-color:rgb(248, 250, 249);
+	display:inline-block;
+	cursor:pointer;
+	color:#934849;
+	font-family:Arial;
+	font-size:13px;
+	font-weight:400;
+	padding: 10.4px;
+  border: none;
+`
+
 const AskQuestion = () => {
+
+  const answerRef1 = useRef();
+  const answerRef2 = useRef();
+  
+  useEffect(()=>{
+    new StacksEditor(answerRef1.current, '', {});
+    new StacksEditor(answerRef2.current, '', {});
+  },[])
+  
+
+  let [title,setTitle] = useState('');
+  let [writing,setWriting] = useState('');
+
+  const typing = (e) => {
+    setWriting(e.target.value);
+    console.log(writing)
+  }
+  const addTodoEnter = (e) => {
+    if(e.key === 'Enter'){
+      addTitle()
+    }
+  }
+  const addTitle = () =>  {
+    setTitle(writing);
+    console.log('타이틀설정',title);
+    setWriting('');
+  }
 
 
 	return (
+    <>
 		<Container>
 			<Header />
       <Box>
@@ -93,8 +260,8 @@ const AskQuestion = () => {
 
         <BlueBox>
           <h2>Writing a good question</h2>
-          <p>You’re ready to ask a programming-related question and this form will help guide you through the process.</p>
-          <p style={{marginBottom:'10px'}}>Looking to ask a non-programming question? See the topics here to find a relevant site.</p>
+          <p>You’re ready to <span className='font-blue'>ask</span> a <span className='font-blue'>programming-related question</span> and this form will help guide you through the process.</p>
+          <p>Looking to ask a non-programming question? See <span className='font-blue'>the topics here</span> to find a relevant site.</p>
         
           <p>steps</p>
           <ul>
@@ -105,8 +272,40 @@ const AskQuestion = () => {
             <li>Review your question and post it to the site.</li>
           </ul>
         </BlueBox>
+
+        <TitleBox>
+          <h5>Title</h5>
+          <p>Be specific and imagine you’re asking a question to another person.</p>
+          <input 
+            type='text' 
+            value={writing}
+            onChange={typing}
+            onKeyUp={addTodoEnter}
+            placeholder='e.g. Is there an R function for finding the index of an element in a vector?'></input>
+          <button onClick={addTitle}>Next</button>
+        </TitleBox>
+        
+        <QuestionBox>
+          <h5>What are the details of your problem?</h5>
+          <p>Introduce the problem and expand on what you put in the title. Minimum 20 characters.</p>
+          <Editor ref={answerRef1} ></Editor>
+        </QuestionBox>
+
+        <QuestionBox>
+          <h5>What did you try and what were you expecting?</h5>
+          <p>Describe what you tried, what you expected to happen, and what actually resulted. Minimum 20 characters.</p>
+          <Editor ref={answerRef2} ></Editor>
+        </QuestionBox>
+
+        <BlueButton>Review your question</BlueButton>
+        <DiscardButton>Discard draft</DiscardButton>
+
         </Box>
+        
 		</Container>
+    <Footer></Footer>
+    </>
+    
 	);
 };
 
