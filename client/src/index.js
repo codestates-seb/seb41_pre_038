f0cd00937ae5d7cbfb6f9177cbfaf9a1b32b4223
JaneChun <janechun22@gmail.com>
2022-12-28 15:55:24 +0900

    redux-persist 추가

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+// - persistStore : 유지하고 싶은 store를 인자로 전달하면, persistor 객체를 반환한다.
+import { persistStore } from 'redux-persist';
+
+export let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>
 );
