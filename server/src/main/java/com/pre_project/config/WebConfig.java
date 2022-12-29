0ba70e2edb1cdfa3a4ad1a45c4bb17a11649afe6
김도형 <maam10@naver.com>
2022-12-29 18:29:38 +0900

    cors 수정 작업자: 김도형

--- server/src/main/java/com/pre_project/config/WebConfig.java
+++ server/src/main/java/com/pre_project/config/WebConfig.java
@@ -11,6 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://ec2-54-180-116-18.ap-northeast-2.compute.amazonaws.com:8080");
+                .allowedOrigins("http://localhost:3000");
     }
 }
