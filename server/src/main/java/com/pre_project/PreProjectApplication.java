8559f43f374f68cfec18c8aa1886e43cfe56c6e1
김도형 <maam10@naver.com>
2022-12-29 17:34:50 +0900

    cors 수정 작업자: 김도형

--- server/src/main/java/com/pre_project/PreProjectApplication.java
+++ server/src/main/java/com/pre_project/PreProjectApplication.java
@@ -16,14 +16,5 @@ public class PreProjectApplication {
 		SpringApplication.run(PreProjectApplication.class, args);
 
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://front-server.com");
-			}
-		};
-	}
 }
 
---
+++ server/src/main/java/com/pre_project/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.pre_project.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://ec2-54-180-116-18.ap-northeast-2.compute.amazonaws.com:8080");
+    }
+}
