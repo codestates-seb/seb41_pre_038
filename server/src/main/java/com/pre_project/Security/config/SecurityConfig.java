53ffe19eea6c67ef9831efccb85f6cfac0cb0e3f
김도형 <maam10@naver.com>
2022-12-30 17:34:18 +0900

    cors 수정 작업자: 김도형

--- server/src/main/java/com/pre_project/Security/config/SecurityConfig.java
+++ server/src/main/java/com/pre_project/Security/config/SecurityConfig.java
@@ -65,11 +65,14 @@ public class SecurityConfig
     {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); //내서버가 응답을 할 때 json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는것
-        config.addAllowedHeader("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.addExposedHeader("authorization");
+        config.setAllowCredentials(true); //내서버가 응답을 할 때 json 을 자바스크립트에서 처리할 수 있게 할지를 설정하는것
+        config.setMaxAge(3600L);
 
-        source.registerCorsConfiguration("*", config);
+        source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
 
@@ -99,4 +102,5 @@ public class SecurityConfig
                     .addFilterAfter(jwtAuthorizationFilter, JwtAuthenticationFilter.class);
         }
     }
+
 }
