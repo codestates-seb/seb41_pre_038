8a216aa9558b3765912bc5bdbf5903622bb9cf76
김도형 <maam10@naver.com>
2022-12-30 14:18:02 +0900

    cors 수정 작업자: 김도형

--- server/src/main/java/com/pre_project/config/CorsConfig.java
+++ server/src/main/java/com/pre_project/config/CorsConfig.java
@@ -9,7 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedMethods("GET","POST","PATCH","DELETE")
-                .allowedOrigins("*");
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
